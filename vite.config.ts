cec18310ed735d4aaef4770a1cb0d8a79474cf78
Lingga Priadi Putra <linggappri@gmail.com>
2025-08-05 14:31:03 +0700

    fix(env): Align everything to VITE_GEMINI_API_KEY standard

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        },
-        plugins: [react()],
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
